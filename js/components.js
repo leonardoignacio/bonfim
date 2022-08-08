4260d1dcd60ba9b226e7f8ec1b680a1a3a00a972
leonardoignacio <82240353+leonardoignacio@users.noreply.github.com>
2022-08-08 17:25:05 -0300

    Add files via upload

--- js/components.js
+++ js/components.js
@@ -1,57 +1,78 @@
 
-class Menu {    
-    constructor(idUl, itens){
+class Menu {
+    constructor(idUl, itens) {
         this.idUl = idUl;
         this.itens = itens;
     }
-    render(){
+    render() {
         let ul = document.getElementById(this.idUl)
-        let menus=""
+        let menus = ""
         this.itens.forEach(i => {
-            menus +=`
+            menus += `
             <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="${i.link}">${i.label}</a>
+                <a  href="${i.link}" class="nav-link clearfix" onclick="toggleMenu()">${i.label}</a>
             </li>
             `
         });
-        ul.innerHTML=menus
-        
+        ul.innerHTML = menus
+
     }
 }
 
-class Carousel {    
-    constructor(idDiv, slids){
+class Carousel {
+    constructor(idDiv, slids) {
         this.idDiv = idDiv;
         this.slids = slids;
     }
-    render(){
+    render() {
         let div = document.getElementById(this.idDiv)
-        let images=""
+        let images = ""
         this.slids.forEach(img => {
-            let active = img.active ? 'active':''
-            images +=`
-                <div class="carousel-item ${active}" data-bs-interval="5000">
-                    <img src="${img.src}" class="d-block w-100 img-fluid" alt="${img.alt}">
+            let active = img.active ? 'active' : ''
+            images += `
+                <div class="carousel-item img-fluid ${active}" data-bs-interval="5000">
+                    <img src="${img.src}" class="rounded mx-auto d-block w-100 img-fluid" alt="${img.alt}">
                 </div>
             `
         });
-        div.innerHTML=images
-        
+        div.innerHTML = images
+
+    }
+}
+
+class CatCarousel {
+    constructor(idDiv, slids) {
+        this.idDiv = idDiv;
+        this.slids = slids;
+    }
+    render() {
+        let div = document.getElementById(this.idDiv)
+        let images = ""
+        this.slids.forEach(img => {
+            let active = img.active ? 'active' : ''
+            images += `
+                <div class="carousel-item img-fluid ${active}" data-bs-interval="5000">
+                    <div class="card" style="width: 18rem;">
+                        <img src="" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h2 class="card-title">Texto</h2>
+                            <p class="card-text">texto</p>
+                        </div>
+                        <div class="card-footer">
+                            <h3>Categoria</h3>
+                        </div>
+                    </div>
+                </div>
+            `
+        });
+        div.innerHTML = images
+
     }
 }
 
 
 /*
 
-<div class="carousel-item active" data-bs-interval="5000">
-<img src="..." class="d-block w-100" alt="...">
-</div>
-<div class="carousel-item" data-bs-interval="2000">
-<img src="..." class="d-block w-100" alt="...">
-</div>
-<div class="carousel-item">
-<img src="..." class="d-block w-100" alt="...">
-</div>
-</div>
+
 
 */
--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 let url=""
+function toggleMenu(){
+    let btnToggleMenu;
+    btnToggleMenu = document.getElementById('btnToggleMenu');
+    if (btnToggleMenu.getAttribute('aria-expanded')){
+        btnToggleMenu.click()
+    }
+    
+}
